2524fefc013f6ce262475ab60a06a73637943fc3
Rishu-Pr <rishum037@gmail.com>
2026-01-30 13:19:28 +0000

    Code challenge submission for question: 268 Missing Number

---
+++ 268 Missing Number/268missing-number.c
@@ -0,0 +1,8 @@
+int missingNumber(int* nums, int numsSize) {
+    int sum = numsSize*(numsSize+1)/2;
+    int sum2=0;
+    for(int i=0;i<numsSize;i++){
+        sum2+= nums[i];
+    }
+    return sum-sum2;
+}
