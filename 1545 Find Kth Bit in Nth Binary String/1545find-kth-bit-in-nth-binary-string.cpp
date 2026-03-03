f91d5d13cb1522b70bdd9561a20b9ab917839b42
Rishu-Pr <rishum037@gmail.com>
2026-03-03 12:43:16 +0000

    Code challenge submission for question: 1545 Find Kth Bit in Nth Binary String

---
+++ 1545 Find Kth Bit in Nth Binary String/1545find-kth-bit-in-nth-binary-string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    char findKthBit(int n, int k)
+    {
+        if (n == 1) return '0';
+
+        int mid = (1 << (n - 1));
+        if (k == mid) return '1';
+        else if (k < mid) return findKthBit(n - 1, k);
+
+        else
+        {
+            int mirror_Idx = mid - (k - mid);
+            char bit = findKthBit(n - 1, mirror_Idx);
+            return (bit == '0') ? '1' : '0';
+        }
+    }
+};
