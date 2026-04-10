da6aaf54f2f3c3465a1f766365f077d5e68c7684
Rishu-Pr <rishum037@gmail.com>
2026-04-10 18:03:54 +0000

    Code challenge submission for question: 2073 Time Needed to Buy Tickets

---
+++ 2073 Time Needed to Buy Tickets/2073time-needed-to-buy-tickets.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        int cnt = 0;
+        for (int i = 0; i < tickets.size(); i++) {
+            if (i <= k)
+                cnt += min(tickets[i], tickets[k]);
+            else
+                cnt += min(tickets[i], tickets[k] - 1);
+        }
+
+        return cnt;
+    }
+};
