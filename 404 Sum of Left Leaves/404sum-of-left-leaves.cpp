a2eebdf38c4e3be0238c6b291b80354e0ba3e674
Rishu-Pr <rishum037@gmail.com>
2026-04-13 23:16:23 +0000

    Code challenge submission for question: 404 Sum of Left Leaves

---
+++ 404 Sum of Left Leaves/404sum-of-left-leaves.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    void addLeft(TreeNode* root,int &sum){
+        if(!root->left){
+            if(root->right) addLeft(root->right,sum);
+            return;
+        }
+        if(!root->left->left && !root->left->right)
+            sum += root->left->val;
+
+        if(root->left) addLeft(root->left,sum);
+        if(root->right) addLeft(root->right,sum);
+    }
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        int sum = 0;
+        addLeft(root,sum);
+        return sum;
+    }
+};
