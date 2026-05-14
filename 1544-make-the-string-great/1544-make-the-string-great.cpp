79043c6bb7891f7ddae4acc5a3ef45aa122b420e
Rishu-Pr <rishum037@gmail.com>
2026-05-14 14:36:41 +0530

    Time: 0 ms (100%), Space: 8.3 MB (96.9%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        string ans = "";
+        for(char c : s){
+            if(!ans.empty() && abs(ans[ans.size() - 1] - c) == 32) ans.pop_back();
+            else ans.push_back(c);
+        }
+
+        return ans;
+    }
+};
