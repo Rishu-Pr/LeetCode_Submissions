22a889bd6cc5afe0595f2065228f9a653b033d3c
Rishu-Pr <rishum037@gmail.com>
2026-05-17 18:11:50 +0530

    Time: 0 ms (100%), Space: 10.2 MB (75.46%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -4,16 +4,19 @@ public:
         int n = nums.size();
         if(n == 0) return 0;
         if(n == 1) return nums[0];
-        vector<int> dp(n, 0);
-        dp[0] = nums[0];
-        dp[1] = max(nums[0],nums[1]);
+        
+        int prev1 = nums[0];
+        int prev2 = max(nums[0],nums[1]);
 
         for(int i = 2; i < n; i++){
-            int inc = dp[i - 2] + nums[i];
-            int exc = dp[i - 1];
-            dp[i] = max(inc, exc);
+            int inc = prev1 + nums[i];
+            int exc = prev2;
+            
+            int curr = max(inc, exc);
+            prev1 = prev2;
+            prev2 = curr;
         }
 
-        return dp[n-1];
+        return prev2;
     }
 };
