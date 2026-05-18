ba7392c310f99c01f1b55120511f1c2335131395
Rishu-Pr <rishum037@gmail.com>
2026-05-18 20:55:31 +0530

    Time: 0 ms (100%), Space: 7.9 MB (64%) - LeetHub

--- 0788-rotated-digits/0788-rotated-digits.cpp
+++ 0788-rotated-digits/0788-rotated-digits.cpp
@@ -2,32 +2,26 @@ class Solution {
 public:
     int rotatedDigits(int n) {
         int count = 0;
-
-        queue<pair<int, bool>> q;
-        q.push({1, false});
-        q.push({2, true});
-        q.push({5, true});
-        q.push({6, true});
-        q.push({8, false});
-        q.push({9, true});
-
-        vector<int> valid_num = {0, 1, 2, 5, 6, 8, 9};
-        vector<bool> is_valid = {false, false, true, true, true, false, true};
-
-        while(!q.empty()){
-            auto [num, is_true] = q.front();
-            q.pop();
-
-            if(num > n) continue;
-            if(is_true) count++;
-
-            for(int i = 0; i < valid_num.size(); i++){
-                int next = num * 10 + valid_num[i];
-                if(next <= n){
-                    int is_allowed = is_true || is_valid[i];
-                    q.push({next, is_allowed});
+        for(int i = 1; i <= n; i++){
+            int num = i;
+            
+            bool is_valid = true;
+            bool is_changeable = false;
+            
+            while(num > 0){
+                int digit = num % 10;
+                if(digit == 3 || digit == 4 || digit == 7){
+                    is_valid = false;
+                    break;
                 }
+
+                if(digit == 2 || digit == 5 || digit == 6 ||digit == 9)
+                    is_changeable = true;
+
+                num /= 10;
             }
+
+            if(is_changeable && is_valid) count++;
         }
 
         return count;
