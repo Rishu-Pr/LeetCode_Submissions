e9f9d56155a2cd88cef980631c5220cf3bc57f8a
Rishu-Pr <rishum037@gmail.com>
2026-05-20 12:36:39 +0530

    Time: 3 ms (93.16%), Space: 31 MB (37.71%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    int solve(vector<vector<char>>& matrix, int i, int j, int& maxVal, vector<vector<int>>& dp){
-        if(i >= matrix.size() || j >= matrix[0].size()) return 0;
-
-        if(dp[i][j] != -1) return dp[i][j];
-        int right = solve(matrix, i, j + 1, maxVal, dp);
-        int diag = solve(matrix, i + 1, j + 1, maxVal, dp);
-        int bottom = solve(matrix, i + 1, j, maxVal, dp);
-
-        if(matrix[i][j] == '1'){
-            dp[i][j] = 1 + min(right, min(diag, bottom));
-            maxVal = max(dp[i][j], maxVal);
-            return dp[i][j];
-        }
-
-        else {
-            dp[i][j] = 0;
-            return dp[i][j];
-        }
-    }
     int maximalSquare(vector<vector<char>>& matrix) {
+        int row = matrix.size();
+        int col = matrix[0].size();
+        
+        vector<vector<int>> dp(row + 1, vector<int>(col + 1, 0));
+
         int maxVal = 0;
-        vector<vector<int>> dp(matrix.size(), vector<int>(matrix[0].size() + 1, -1));
-        solve(matrix, 0, 0, maxVal, dp);
+        for(int i = row - 1; i >= 0; i-- ){
+            for(int j = col - 1; j >= 0; j--){        
+                int right = dp[i][j + 1];
+                int diag = dp[i + 1][j + 1];
+                int bottom = dp[i + 1][j];
+
+                if(matrix[i][j] == '1'){
+                    dp[i][j] = 1 + min(right, min(diag, bottom));
+                    maxVal = max(dp[i][j], maxVal);
+                }
+                else dp[i][j] = 0;
+            }
+        }
+
         return maxVal * maxVal;
     }
 };
