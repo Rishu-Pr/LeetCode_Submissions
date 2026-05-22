53ef89196f29ff5bca19318042ca5575198edd52
Rishu-Pr <rishum037@gmail.com>
2026-05-22 23:45:06 +0530

    Time: 6 ms (52.79%), Space: 12.4 MB (56.5%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -3,17 +3,20 @@ public:
     int maxSatisfaction(vector<int>& satisfaction) {
         int n = satisfaction.size();
         sort(satisfaction.begin(), satisfaction.end());
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+        vector<int> prev(n + 1, 0);
+        vector<int> curr(n + 1, 0);
 
         for(int i = n - 1; i >= 0; i--){
 
             for(int j = i; j >= 0; j--){
-                int inc = satisfaction[i] * (j + 1) + dp[i + 1][j + 1];
-                int exc = dp[i + 1][j];
-                dp[i][j] = max(inc, exc);
+                int inc = satisfaction[i] * (j + 1) + prev[j + 1];
+                int exc = prev[j];
+                curr[j] = max(inc, exc);
             }
+
+            prev = curr;
         }
 
-        return dp[0][0];
+        return prev[0];
     }
 };
