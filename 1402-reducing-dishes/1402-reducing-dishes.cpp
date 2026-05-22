da650a77f00a5b1022666a4efa02dd89547a9e73
Rishu-Pr <rishum037@gmail.com>
2026-05-22 19:29:21 +0530

    Time: 0 ms (100%), Space: 11.4 MB (95.49%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -3,7 +3,7 @@ public:
     int maxSatisfaction(vector<int>& satisfaction) {
         int n = satisfaction.size();
 
-        vector<int> ans(n, 0);
+        int maxVal = INT_MIN;
         sort(satisfaction.begin(), satisfaction.end());
 
         for(int i = 0; i < n; i++){
@@ -13,10 +13,9 @@ public:
                 sum += satisfaction[j] * idx;
                 idx++;
             }
-            ans[i] = sum;
+            maxVal = max(maxVal, sum);
         }
-        sort(ans.begin(), ans.end());
         
-        return (ans.back() <= 0) ? 0 : ans.back();
+        return (maxVal <= 0) ? 0 : maxVal;
     }
 };
