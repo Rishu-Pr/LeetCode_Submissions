9bded9c8cad6abef065a406a0e60d6288d0d0c83
Rishu-Pr <rishum037@gmail.com>
2026-05-23 09:39:32 +0530

    Time: 287 ms (26.31%), Space: 296.4 MB (17.62%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,21 +1,20 @@
 class Solution {
-    int solve(vector<int> &nums, int prev, int curr, vector<vector<int>>& dp){
-        if(curr == nums.size()) return 0;
-        if(dp[curr][prev + 1] != -1) return dp[curr][prev + 1];
-
-        int inc = 0;
-        if(prev == -1 || nums[prev] < nums[curr]) inc = 1 + solve(nums, curr, curr + 1, dp);
-
-        int exc = solve(nums, prev, curr + 1, dp);
-
-        dp[curr][prev + 1] = max(inc, exc);
-        return dp[curr][prev + 1];
-    }
 public:
     int lengthOfLIS(vector<int> &nums)
     {
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
-        return solve(nums, -1, 0, dp);
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+
+        for(int curr = n - 1; curr >= 0; curr--){
+            for(int prev = curr - 1; prev >= -1; prev--){
+                int inc = 0; 
+                if(prev == -1 || nums[prev] < nums[curr]) inc = 1 + dp[curr + 1][curr + 1];
+                int exc = dp[curr + 1][prev + 1];
+
+                dp[curr][prev + 1] = max(inc, exc);
+            }
+        }
+
+        return dp[0][0];
     }
 };
