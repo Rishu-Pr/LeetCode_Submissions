34b61664d40ac59ed7f20ffb15a80ccdaf8221b3
Rishu-Pr <rishum037@gmail.com>
2026-05-25 23:15:12 +0530

    Time: 11 ms (61.81%), Space: 27.3 MB (8.02%) - LeetHub

--- 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.cpp
+++ 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.cpp
@@ -1,22 +1,28 @@
 class Solution {
-    int solve(vector<int>& slices, int start, int end, int slices_remains, vector<vector<int>>& dp){
-        if(slices_remains == 0 || start > end) return 0;
-        if(dp[start][slices_remains] != -1) return dp[start][slices_remains];
-
-        int take = slices[start] + solve(slices, start + 2, end, slices_remains - 1, dp);
-        int notTake = solve(slices, start + 1, end, slices_remains, dp);
-
-        return dp[start][slices_remains] = max(take, notTake);
-    }
 public:
     int maxSizeSlices(vector<int>& slices) {
         int size = slices.size();
-        vector<vector<int>> dp1(size + 1, vector<int>(size + 1, -1));
-        vector<vector<int>> dp2(size + 1, vector<int>(size + 1, -1));
+        vector<vector<int>> dp1(size + 2, vector<int>(size + 1, 0));
+        vector<vector<int>> dp2(size + 2, vector<int>(size + 1, 0));
         
-        int firstSlice_inc = solve(slices, 0, size - 2, size / 3, dp1);
-        int firstSlice_exc = solve(slices, 1, size - 1, size / 3, dp2);
+        for(int i = size - 2; i >= 0; i--){
+            for(int j = 1; j <= size / 3; j++){
+                int take = slices[i] + dp1[i + 2][j - 1];
+                int notTake = dp1[i + 1][j];
 
-        return max(firstSlice_inc, firstSlice_exc);
+                dp1[i][j] = max(take, notTake);
+            }
+        }
+        
+        for(int i = size - 1; i >= 1; i--){
+            for(int j = 1; j <= size / 3; j++){
+                int take = slices[i] + dp2[i + 2][j - 1];
+                int notTake = dp2[i + 1][j];
+
+                dp2[i][j] = max(take, notTake);
+            }
+        }
+
+        return max(dp1[0][size / 3], dp2[1][size / 3]);
     }
 };
