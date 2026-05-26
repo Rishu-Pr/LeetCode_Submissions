c33b58d8e113e2bc76c224c7755f636c24c46d6c
Rishu-Pr <rishum037@gmail.com>
2026-05-26 19:35:32 +0530

    Time: 4 ms (99.26%), Space: 15 MB (73.85%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,25 +1,21 @@
 class Solution {
-    bool isfirstHalf(vector<int>& nums, int idx, int total, vector<vector<int>>& dp){
-        if(total == 0) return true;
-        if(total < 0 || idx >= nums.size()) return false;
-        
-        if(dp[idx][total] != -1) return dp[idx][total];
-
-        bool ans = false;
-
-        for(int i = idx; i < nums.size(); i++)
-            if(isfirstHalf(nums, i + 1, total - nums[i], dp)) 
-                return dp[idx][total] = 1;
-        
-        return dp[idx][total] = 0;
-    }
 public:
     bool canPartition(vector<int>& nums) {
         int sum = 0;
         for(int i = 0; i < nums.size(); i++) sum += nums[i];
         if(sum % 2) return false;
 
-        vector<vector<int>> dp(nums.size(), vector<int>(sum / 2 + 1, -1));
-        return isfirstHalf(nums, 0, sum / 2, dp);
+        vector<int> dp(sum / 2 + 1, 0);
+        dp[0] = 1;
+
+        for(int i = 0; i < nums.size(); i++){
+            for(int j = sum / 2; j >= 0; j--){
+                if(dp[j]){
+                    if(j + nums[i] <= sum / 2) dp[j + nums[i]] = 1;
+                    if(dp[sum / 2]) return true;
+                }
+            }
+        }
+        return false;
     }
 };
