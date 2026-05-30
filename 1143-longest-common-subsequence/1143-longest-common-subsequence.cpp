eeed248719f241556f3b89db399a4adc747c55e0
Rishu-Pr <rishum037@gmail.com>
2026-05-30 22:29:06 +0530

    Time: 31 ms (33.79%), Space: 27.4 MB (67.54%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,18 +1,20 @@
 class Solution {
-    int solve(string& text1, string& text2, int i, int j,vector<vector<int>>& dp){
-        if(i >= text1.size() || j >= text2.size()) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-
-        int matches = 0;
-        int skip1 = solve(text1, text2, i + 1, j, dp);
-        int skip2 = solve(text1, text2, i, j + 1, dp);
-        if(text1[i] == text2[j]) matches = 1 + solve(text1, text2, i + 1, j + 1, dp);
-
-        return dp[i][j] = max(matches, max(skip1, skip2));
-    }
 public:
     int longestCommonSubsequence(string text1, string text2) {
-        vector<vector<int>> dp(text1.size(), vector<int>(text2.size(), -1));
-        return solve(text1, text2, 0, 0, dp);
+        int n = text1.size();
+        int m = text2.size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+        for(int i = m - 1; i >= 0; i--){
+            for(int j = n - 1; j >= 0; j--){
+                int matches = (text1[j] == text2[i]) ? 1 + dp[i + 1][j + 1] : 0;
+                int skip1 = dp[i + 1][j];
+                int skip2 = dp[i][j + 1];
+
+                dp[i][j] = max(matches, max(skip1, skip2));
+            }
+        }
+
+        return dp[0][0];
     }
 };
