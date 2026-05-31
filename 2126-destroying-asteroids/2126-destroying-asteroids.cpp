93113bac6459e29684b9616996378dec88002d0d
Rishu-Pr <rishum037@gmail.com>
2026-05-31 23:24:44 +0530

    Time: 43 ms (10.07%), Space: 106.7 MB (81.61%) - LeetHub

---
+++ 2126-destroying-asteroids/2126-destroying-asteroids.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        sort(asteroids.begin(), asteroids.end());
+
+        if(mass < asteroids[0]) return false;
+        asteroids[0] += mass;
+        if(asteroids.size() > 1 && asteroids[0] < asteroids[1]) return false;
+
+        for(int i = 1; i < asteroids.size() - 1; i++){
+            if(asteroids[i] > 100000 || asteroids[i - 1] > 100000) return true;
+            asteroids[i] += asteroids[i - 1];
+            if(asteroids[i] < asteroids[i + 1]) return false;
+        }
+
+        return true;
+    }
+};
