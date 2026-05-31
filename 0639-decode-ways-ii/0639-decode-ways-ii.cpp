cf74ac9bdecc275a58d0b48abda0e8e3eff27b00
Rishu-Pr <rishum037@gmail.com>
2026-05-31 15:27:34 +0530

    Time: 2333 ms (5.17%), Space: 58.2 MB (5.17%) - LeetHub

--- 0639-decode-ways-ii/0639-decode-ways-ii.cpp
+++ 0639-decode-ways-ii/0639-decode-ways-ii.cpp
@@ -1,28 +1,33 @@
 #define MOD 1000000007
 class Solution {
-    int solve(string& s, int idx,vector<long long>& dp){
+    long long solve(string& s, int idx, vector<long long>& dp){
         int n = s.size();
         if(idx == n) return 1;
-        if(idx > n || s[idx] == '0') return 0;
-        if(dp[idx] != -1) return dp[idx] % MOD;
+        if(s[idx] == '0') return 0;
+        if(dp[idx] != -1) return dp[idx];
 
-        if (s[idx] == '*' && s[idx + 1] == '*') return dp[idx] = (96 * (solve(s, idx + 2, dp) % MOD)) % MOD;
-        if(s[idx] == '*') {
-            int sum = 0;
-            if(idx < n - 1 && s[idx + 1] >= '1' && s[idx + 1] <= '6') sum += 2;
-            else if(idx < n - 1 && s[idx + 1] >= '7' && s[idx + 1] <= '9') sum += 1;
-            return dp[idx] = ((9 + sum) * (solve(s, idx + 1, dp) % MOD)) % MOD;
+        long long ways = 0;
+
+        if(s[idx] == '*') ways = (9 * solve(s, idx+1, dp)) % MOD;
+        else ways = solve(s, idx+1, dp) % MOD;
+
+        if(idx+1 < n) {
+            if(s[idx] == '*' && s[idx+1] == '*') ways = (ways + 15 * solve(s, idx+2, dp)) % MOD;
+            else if(s[idx] == '*') {
+                if(s[idx+1] >= '0' && s[idx+1] <= '6') ways = (ways + 2 * solve(s, idx+2, dp)) % MOD;
+                else if(s[idx+1] >= '7' && s[idx+1] <= '9') ways = (ways + solve(s, idx+2, dp)) % MOD;
+            }
+            else if(s[idx+1] == '*') {
+                if(s[idx] == '1') ways = (ways + 9 * solve(s, idx+2, dp)) % MOD;
+                else if(s[idx] == '2') ways = (ways + 6 * solve(s, idx+2, dp)) % MOD;
+            }
+            else {
+                int val = stoi(s.substr(idx, 2));
+                if(val >= 10 && val <= 26) ways = (ways + solve(s, idx+2, dp)) % MOD;
+            }
         }
 
-        int step_1 = solve(s, idx + 1, dp);
-        int step_2 = 0;
-        if(idx <= n - 2){
-            if(s[idx + 1] == '*' && s[idx] == '1') step_2 = (9 * (solve(s, idx + 2, dp) % MOD)) % MOD;
-            else if(s[idx + 1] == '*' && s[idx] == '2') step_2 = (6 * (solve(s, idx + 2, dp) % MOD)) % MOD;
-            else if(stoi(s.substr(idx, 2)) <= 26 && s[idx + 1] != '*') step_2 = solve(s, idx + 2, dp);
-        }
-        
-        return dp[idx] =  (step_1 + step_2) % MOD;
+        return dp[idx] = ways % MOD;
     }
 public:
     int numDecodings(string s) {
