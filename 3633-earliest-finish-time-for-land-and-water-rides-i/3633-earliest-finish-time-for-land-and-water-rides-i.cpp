0078b8e3328dcba0c6e7d06eb51f2340416d1e7f
Rishu-Pr <rishum037@gmail.com>
2026-06-02 10:29:58 +0530

    Time: 0 ms (100%), Space: 91.9 MB (24.47%) - LeetHub

--- 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.cpp
+++ 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.cpp
@@ -1,35 +1,20 @@
 class Solution {
+    int solve(vector<int>& FirstStartTime, vector<int>& FirstDuration, vector<int>& secondStartTime, vector<int>& secondDuration){
+        int endRide1 = INT_MAX;
+        for(int i = 0; i < FirstStartTime.size(); i++)
+            endRide1 = min(endRide1, FirstStartTime[i] + FirstDuration[i]);
+
+        int endRide2 = INT_MAX;
+        for(int i = 0; i < secondStartTime.size(); i++)
+            endRide2 = min(endRide2, max(secondStartTime[i], endRide1) + secondDuration[i]);
+
+        return endRide2;
+    }
 public:
     int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
-        vector<vector<int>> landFirst(landStartTime.size(), vector<int>(waterStartTime.size(), 0));
-        vector<vector<int>> waterFirst(waterStartTime.size(), vector<int>(landStartTime.size(), 0));
+        int landFirst = solve(landStartTime, landDuration, waterStartTime, waterDuration);
+        int waterFirst = solve(waterStartTime, waterDuration, landStartTime, landDuration);
 
-        for(int i = 0; i < landStartTime.size(); i++){
-            for(int j = 0; j < waterStartTime.size(); j++){
-                landFirst[i][j] = landStartTime[i] + landDuration[i];
-                if(landFirst[i][j] < waterStartTime[j]) landFirst[i][j] = waterStartTime[j] + waterDuration[j];
-                else landFirst[i][j] += waterDuration[j];
-            }
-        }
-        for(int i = 0; i < waterStartTime.size(); i++){
-            for(int j = 0; j < landStartTime.size(); j++){
-                waterFirst[i][j] =waterStartTime[i] + waterDuration[i];
-                if(waterFirst[i][j] < landStartTime[j]) waterFirst[i][j] = landStartTime[j] + landDuration[j];
-                else waterFirst[i][j] += landDuration[j];
-            }
-        }
-        int minVal = landFirst[0][0];
-        for(int i = 0; i < landFirst.size(); i++){
-            for(int j = 0; j < landFirst[i].size(); j++){
-                minVal = min(minVal, landFirst[i][j]);
-            }
-        }
-        for(int i = 0; i < waterFirst.size(); i++){
-            for(int j = 0; j < waterFirst[i].size(); j++){
-                minVal = min(minVal, waterFirst[i][j]);
-            }
-        }
-
-        return minVal;
+        return min(landFirst, waterFirst);
     }
 };
