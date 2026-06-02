655bd788e60abb2e8d6747f48c8c80f4c923f025
Rishu-Pr <rishum037@gmail.com>
2026-06-02 23:36:46 +0530

    Time: 16 ms (79.32%), Space: 9.1 MB (85.28%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int numDistinct(string s, string t) {
-        vector<vector<long long>> dp(s.size() + 1, vector<long long>(t.size() + 1, 0));
-        for(int i = 0; i <= s.size(); i++) dp[i][t.size()] = 1;
+        vector<unsigned long long> prev(t.size() + 1, 0); prev[t.size()] = 1;
+        vector<unsigned long long> curr(t.size() + 1, 0); curr[t.size()] = 1;
 
         for(int idx_s = s.size() - 1; idx_s >= 0; idx_s--){
             for(int idx_T = t.size() - 1; idx_T >= 0; idx_T--){
-                if(s[idx_s] == t[idx_T]) dp[idx_s][idx_T] = dp[idx_s + 1][idx_T] + dp[idx_s + 1][idx_T + 1];
-                else dp[idx_s][idx_T] = dp[idx_s + 1][idx_T];
+                if(s[idx_s] == t[idx_T]) curr[idx_T] = prev[idx_T] + prev[idx_T + 1];
+                else curr[idx_T] = prev[idx_T];
             }
+
+            prev = curr;
         }
-        return dp[0][0];
+        return prev[0];
     }
 };
