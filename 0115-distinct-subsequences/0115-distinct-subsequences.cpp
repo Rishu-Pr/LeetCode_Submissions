1e720d6f26b16d2e29fd845cf8ffcc70a89b1bec
Rishu-Pr <rishum037@gmail.com>
2026-06-02 23:31:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,19 +1,15 @@
 class Solution {
-    int solve(string& s, string& t, int idx_s, int idx_T, vector<vector<int>>& dp){
-        if(idx_T == t.size()) return 1;
-        if(idx_s == s.size() && idx_T < t.size()) return 0;
-        if(dp[idx_s][idx_T] != -1) return dp[idx_s][idx_T];
-
-        int matches = 0;
-        if(s[idx_s] == t[idx_T]) matches = solve(s, t, idx_s + 1, idx_T + 1, dp);
-
-        int skip = solve(s, t, idx_s + 1, idx_T, dp);
-
-        return dp[idx_s][idx_T] = matches + skip;
-    }
 public:
     int numDistinct(string s, string t) {
-        vector<vector<int>> dp(s.size(), vector<int>(t.size(), -1));
-        return solve(s, t, 0, 0, dp);
+        vector<vector<long long>> dp(s.size() + 1, vector<long long>(t.size() + 1, 0));
+        for(int i = 0; i <= s.size(); i++) dp[i][t.size()] = 1;
+
+        for(int idx_s = s.size() - 1; idx_s >= 0; idx_s--){
+            for(int idx_T = t.size() - 1; idx_T >= 0; idx_T--){
+                if(s[idx_s] == t[idx_T]) dp[idx_s][idx_T] = dp[idx_s + 1][idx_T] + dp[idx_s + 1][idx_T + 1];
+                else dp[idx_s][idx_T] = dp[idx_s + 1][idx_T];
+            }
+        }
+        return dp[0][0];
     }
 };
