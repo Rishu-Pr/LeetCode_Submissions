7060c35770bd4e839ee7bbfb9d899c0eb9039387
Rishu-Pr <rishum037@gmail.com>
2026-06-05 10:33:57 +0530

    Time: 3 ms (78.83%), Space: 11.5 MB (43.05%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:    
     int nthUglyNumber(int n) {
-        vector<int> V = {1};
-        
+        vector<int> V(n, 0);
+        V[0] = 1;
         int ptr_2 = 0;
         int ptr_3 = 0;
         int ptr_5 = 0;
+        int idx = 1;
 
-        while(V.size() < n){
+        while(idx < n){
             int Val_2 = V[ptr_2] * 2;
             int Val_3 = V[ptr_3] * 3;
             int Val_5 = V[ptr_5] * 5;
 
-            int minV = min(Val_2, min(Val_3, Val_5));
-            V.push_back(minV);
+            int minV = min({Val_2, Val_3, Val_5});
+            V[idx] = minV; idx++;
 
             if(minV == Val_2) ptr_2++;
             if(minV == Val_3) ptr_3++;
