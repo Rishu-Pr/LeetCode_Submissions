a31ddbde8e53265b5d594bc8feed70f7fddd7d92
Rishu-Pr <rishum037@gmail.com>
2026-06-06 19:32:12 +0530

    Time: 0 ms (100%), Space: 7.8 MB (55.02%) - LeetHub

--- 0397-integer-replacement/0397-integer-replacement.cpp
+++ 0397-integer-replacement/0397-integer-replacement.cpp
@@ -1,15 +1,16 @@
 class Solution {
-    int solve(int n, unordered_map<long long, int>& dp){
-        if(n == 1) return 0;
-        if(dp.find(n) != dp.end()) return dp[n];
-
-        if(n % 2 == 0) return dp[n] = 1 + solve(n / 2, dp);
-        else return dp[n] = 1 + min(solve(n - 1, dp), solve(n + 1, dp));
-    }
 public:
     int integerReplacement(int n) {
-        if(n == 2147483647) return 32;
-        unordered_map<long long, int> dp;
-        return solve(n, dp);
+        long long num = n;
+        int oprn = 0;
+        while(num > 1){
+            if(!(num % 2)) num = num >> 1;
+            else if(num == 3 || (num % 4) == 1) num--;
+            else num++;
+
+            oprn++;
+        }
+
+        return oprn;
     }
 };
