267d3ac40be9dc5d39e0f53ec0e045aabe5c77e5
Rishu-Pr <rishum037@gmail.com>
2026-06-06 13:33:35 +0530

    Time: 11 ms (6.2%), Space: 99.6 MB (77.76%) - LeetHub

--- 0396-rotate-function/0396-rotate-function.cpp
+++ 0396-rotate-function/0396-rotate-function.cpp
@@ -2,21 +2,20 @@ class Solution {
 public:
     int maxRotateFunction(vector<int>& nums) {
         if(nums.size() == 1) return 0;
-        vector<int> dp(nums.size(), 0);
         int sum = 0;
+        int curr = 0;
+        int maxV = 0;
+
         for(int i = 0; i < nums.size(); i++) {
             sum += nums[i];
+            curr += i * nums[i];
         }
+        maxV = curr;
 
-        for(int i = 0; i < nums.size(); i++) {
-            dp[0] += i * nums[i];
+        for(int i = 1; i < nums.size(); i++){ 
+            curr = curr + (nums.size() * nums[i - 1]) - sum;
+            maxV = max(maxV, curr);
         }
-
-        for(int i = 1; i < nums.size(); i++) 
-            dp[i] = dp[i - 1] + (nums.size() * nums[i - 1]) - sum;
-
-        int maxV = dp[0];
-        for(int i = 1; i < dp.size(); i++) maxV = max(maxV, dp[i]); 
         return maxV;
     }
 };
