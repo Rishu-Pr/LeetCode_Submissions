2aa06eed3e74c32b9a1750539967ad101080c574
Rishu-Pr <rishum037@gmail.com>
2026-06-09 01:58:32 +0530

    Time: 0 ms (100%), Space: 8.1 MB (26.14%) - LeetHub

--- 1611-minimum-one-bit-operations-to-make-integers-zero/1611-minimum-one-bit-operations-to-make-integers-zero.cpp
+++ 1611-minimum-one-bit-operations-to-make-integers-zero/1611-minimum-one-bit-operations-to-make-integers-zero.cpp
@@ -2,17 +2,10 @@ class Solution {
 public:
     int minimumOneBitOperations(int n) {
         int ans = 0;
-        int cnt = 1;
-        int sign = 1;
         while(n > 0){
-            if(n % 2){
-                ans += (pow(2, cnt) - 1) * sign;
-                sign *= -1;
-            }
-            n /= 2;
-            cnt++;
+            ans ^= n;
+            n >>= 1;
         }
-
-        return abs(ans);
+        return ans;
     }
 };
