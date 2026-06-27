42df9c028a8ad3a65997ff51b2029baa11fe057f
Rishu-Pr <rishum037@gmail.com>
2026-06-27 11:46:25 +0530

    Time: 67 ms (89.25%), Space: 98.8 MB (33.37%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,30 +1,13 @@
 class Solution {
-    static bool cmp(const pair<int, int>& a, const pair<int, int>& b) {
-        if (a.first == b.first) {
-            return a.second < b.second;
-        }
-        return a.first < b.first;
-    }
-
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        vector<pair<int, int>> map;
-        for (int i = 0; i < nums.size(); i++) {
-            map.push_back({nums[i], i});
-        }
+        unordered_map<int, int> map;
 
-        sort(map.begin(), map.end(), cmp);
-
-        int s = 0;
-        int e = 1;
-        
-        while (e < map.size()) {
-            if (map[s].first == map[e].first && (map[e].second - map[s].second) <= k) {
+        for(int i = 0; i < nums.size(); i++){
+            if(map.count(nums[i]) && i - map[nums[i]] <= k)
                 return true;
-            }
-            
-            s++;
-            e++;
+
+            map[nums[i]] = i;
         }
 
         return false;
