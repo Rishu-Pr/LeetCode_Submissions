36dd60cd6c1cb079d2803738bb43a85736f6848c
Rishu-Pr <rishum037@gmail.com>
2026-06-27 14:14:35 +0530

    Time: 0 ms (100%), Space: 9.8 MB (25.56%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,8 +1,22 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return s == t;
+        if(s.size() != t.size()){
+            return false;
+        }
+
+        vector<int> freq(26, 0);
+        for(char chr : s){
+            freq[chr - 'a']++;
+        }
+        for(char chr : t){
+            freq[chr - 'a']--;
+        }
+
+        for(int i = 0; i < freq.size(); i++){
+            if(freq[i] != 0) return false;
+        }
+
+        return true;
     }
 };
