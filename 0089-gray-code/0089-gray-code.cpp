56124f9d28bc4f45e80ababb50ee1eb38e3616ba
Rishu-Pr <rishum037@gmail.com>
2026-07-10 02:53:04 +0530

    Time: 7 ms (10.55%), Space: 14.6 MB (74.58%) - LeetHub

---
+++ 0089-gray-code/0089-gray-code.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        vector<int> ans = {0};
+        for(int i = 0; i < n; i++){
+            int shift = 1 << i;
+
+            for(int j = ans.size() - 1; j >= 0; j--){
+                ans.push_back(ans[j] + shift);
+            }
+        }
+
+        return ans;
+    }
+};
