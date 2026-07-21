e9c23e97f6e0dc5f0ab465c541eb3c3e6bbfd970
Rishu-Pr <rishum037@gmail.com>
2026-07-21 17:42:57 +0530

    Time: 0 ms (100%), Space: 7.9 MB (2.84%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.cpp
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -1,17 +1,6 @@
 class Solution {
 public:
     int bulbSwitch(int n) {
-        if(n == 0){
-            return 0;
-        }
-        if(n == 1){
-            return 1;
-        }
-        long long ans = n / 2;
-        while(ans * ans > n){
-            ans--;
-        }
-
-        return ans;
+        return sqrt(n);
     }
 };
