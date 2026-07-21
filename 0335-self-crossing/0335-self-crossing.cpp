3c4def60c8e7cd0110dbf63733fa3b283bfeffe4
Rishu-Pr <rishum037@gmail.com>
2026-07-22 01:13:39 +0530

    Time: 0 ms (100%), Space: 30 MB (56.26%) - LeetHub

---
+++ 0335-self-crossing/0335-self-crossing.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isSelfCrossing(vector<int>& distance) {
+        if(distance.size() <= 3){
+            return false;
+        }
+        for(int i = 3; i < distance.size(); i++){
+            if(distance[i - 1] <= distance[i - 3] && distance[i] >= distance[i - 2]){
+                return true;
+            }
+
+            if(i >= 4 && distance[i - 1] == distance[i - 3] && distance[i - 2] <= distance[i - 4] + distance[i]){
+                return true;
+            }
+
+            if(i >= 5 && distance[i - 2] >= distance[i - 4] && distance[i - 1] <= distance[i - 3] && distance[i - 1] + distance[i - 5] >= distance[i - 3] && distance[i] + distance[i - 4] >= distance[i - 2]){
+                return true;
+            }
+
+        }
+        return false;
+    }
+};
