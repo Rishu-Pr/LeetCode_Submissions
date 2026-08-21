045567505ab326015062c60dfa06b461b2933419
Rishu-Pr <rishum037@gmail.com>
2026-08-21 17:46:58 +0530

    Time: 1 ms (17.78%), Space: 12.2 MB (63.91%) - LeetHub

---
+++ 0274-h-index/0274-h-index.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        if(citations.size() == 1 && citations[0] < 1){
+            return 0;
+        }
+        sort(citations.begin(), citations.end());
+        
+        int count = 1;
+
+        for(int i = citations.size() - 1; i >= 0; i--){
+            if(count <= citations[i]){
+                count++;
+            }
+            else break;
+        }
+
+        return (count - 1);
+    }
+};
