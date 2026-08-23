6e43b8609aa5b40844d7ac8da2ad00e67495c4b2
Rishu-Pr <rishum037@gmail.com>
2026-08-23 22:44:35 +0530

    Time: 4 ms (47.55%), Space: 14.1 MB (33.57%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool sumGame(string num) {
+        int leftsum = 0;
+        int left_blank = 0;
+        int rightsum = 0;
+        int right_blank = 0;
+
+        int idx1 = num.size() / 2;
+        for(int i = 0; i < idx1; i++){
+            if(num[i] == '?'){
+                left_blank++;
+            }
+            else{
+                leftsum += num[i] - '0';
+            }
+        }
+        for(int i = idx1; i < num.size(); i++){
+            if(num[i] == '?'){
+                right_blank++;
+            }
+            else{
+                rightsum += num[i] - '0';
+            }
+        }
+
+        if(left_blank - right_blank == 0){
+            return (!(leftsum == rightsum));
+        }
+        if((left_blank + right_blank) % 2){
+            return true;
+        }
+        else{
+            int netblank = abs(left_blank - right_blank);
+            int netsum = abs(leftsum - rightsum);
+            if(((left_blank - right_blank)*(leftsum - rightsum) < 0) && (netblank / 2) * 9 == netsum){
+                return false;
+            }
+            return true;
+        }
+
+    }
+};
