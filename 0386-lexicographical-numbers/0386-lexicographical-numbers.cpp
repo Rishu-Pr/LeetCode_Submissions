0d70269310bdd160edf65c24d065b7df629f5df2
Rishu-Pr <rishum037@gmail.com>
2026-09-02 03:07:38 +0530

    Time: 75 ms (11.07%), Space: 28.2 MB (7.38%) - LeetHub

---
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        vector<string> temp;
+        for(int i = 1; i <= n; i++){
+            temp.push_back(to_string(i));
+        }
+        sort(temp.begin(), temp.end());
+        vector<int> ans;
+        for(string s : temp){
+            ans.push_back(stoi(s));
+        }
+
+        return ans;
+    }
+};
