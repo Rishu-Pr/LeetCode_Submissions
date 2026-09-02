d9780ab0ffe9c577f4a5c82339afc621927791b1
Rishu-Pr <rishum037@gmail.com>
2026-09-02 13:54:03 +0530

    Time: 0 ms (100%), Space: 10.6 MB (55.5%) - LeetHub

--- 0667-beautiful-arrangement-ii/0667-beautiful-arrangement-ii.cpp
+++ 0667-beautiful-arrangement-ii/0667-beautiful-arrangement-ii.cpp
@@ -4,29 +4,36 @@ public:
         vector<int> V;
         int l = 1;
         int r = n;
-        if(k == 1){
-            for(int i = 1; i <= n; i++){
-                V.push_back(i);
-            }
-            return V;
-        }
+        // if(k == 1){
+        //     for(int i = 1; i <= n; i++){
+        //         V.push_back(i);
+        //     }
+        //     return V;
+        // }
 
-        V.push_back(l++);
-        while(k > 1){
-            V.push_back(r--);
-            k--;
+        // V.push_back(l++);
+        // while(k > 1){
+        //     V.push_back(r--);
+        //     k--;
+        //     if(k > 1){
+        //         V.push_back(l++);
+        //         k--;
+        //     }
+        // }
+        // if(V.back() == r + 1){
+        //     while(r >= l){
+        //         V.push_back(r--);
+        //     }
+        // }else if(V.back() == l - 1){
+        //     while(r >= l){
+        //         V.push_back(l++);
+        //     }
+        // }
+        while(l <= r){
+            int val = (k % 2 ? l++ : r--);
+            V.push_back(val);
             if(k > 1){
-                V.push_back(l++);
                 k--;
-            }
-        }
-        if(V.back() == r + 1){
-            while(r >= l){
-                V.push_back(r--);
-            }
-        }else if(V.back() == l - 1){
-            while(r >= l){
-                V.push_back(l++);
             }
         }
 
