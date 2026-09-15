596e14bc2b3bf2bff98321d3161e57e8832bd2d3
Rishu-Pr <rishum037@gmail.com>
2026-09-15 17:23:35 +0530

    Time: 8 ms (91.35%), Space: 139 MB (92.31%) - LeetHub

--- 3290-maximum-multiplication-score/3290-maximum-multiplication-score.cpp
+++ 3290-maximum-multiplication-score/3290-maximum-multiplication-score.cpp
@@ -1,22 +1,14 @@
 class Solution {
-    long long solve(vector<int>& a, vector<int>& b, int idx1, int idx2, vector<vector<long long>>& dp){
-        if(idx1 >= a.size()){
-            return 0;
-        }
-        if(idx1 < a.size() && idx2 >= b.size()){
-            return -1e15;
-        }
-
-        if(dp[idx1][idx2] != -1) return dp[idx1][idx2];
-        
-        long long val = (long long)a[idx1] * b[idx2] + solve(a, b, idx1 + 1, idx2 + 1, dp);
-        val = max(val, solve(a, b, idx1, idx2 + 1, dp));
-
-        return dp[idx1][idx2] = val;
-    }
 public:
     long long maxScore(vector<int>& a, vector<int>& b) {
-        vector<vector<long long>> dp(a.size(), vector<long long>(b.size(), -1));
-        return solve(a, b, 0, 0, dp);
+        vector<long long> dp(4, -1e15);
+        for(long long x : b){
+            dp[3] = max(dp[3], dp[2] + x * a[3]);
+            dp[2] = max(dp[2], dp[1] + x * a[2]);
+            dp[1] = max(dp[1], dp[0] + x * a[1]);
+            dp[0] = max(dp[0], x * a[0]);
+        }
+
+        return dp[3];
     }
 };
